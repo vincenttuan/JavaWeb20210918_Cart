236574bb63119c12602939c71abe7c48e1d5ad5e
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-10-23 11:43:06 +0800

    Add files via upload

---
+++ src/main/java/sql/insert_sample_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO users(name, password) VALUES('admin', '1111');
+INSERT INTO users(name, password) VALUES('john', '2222');
+INSERT INTO users(name, password) VALUES('mary', '3333');
+
+INSERT INTO products(name, qty, price) VALUES('鉛筆', 10, 10);
+INSERT INTO products(name, qty, price) VALUES('橡皮', 10, 20);
+INSERT INTO products(name, qty, price) VALUES('墊板', 30, 20);
+INSERT INTO products(name, qty, price) VALUES('圓規', 40, 30);
+INSERT INTO products(name, qty, price) VALUES('彈珠', 50, 10);
